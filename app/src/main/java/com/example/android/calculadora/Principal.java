c5b180af8ba4ebbeb91154c1f50702198474ef8f
David <dacero1@cuc.edu.co>
2017-09-16 12:47:40 -0500

    Inicio de la codificacion

--- app/src/main/java/com/example/android/calculadora/Principal.java
+++ app/src/main/java/com/example/android/calculadora/Principal.java
@@ -1,13 +1,31 @@
 package com.example.android.calculadora;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class Principal extends AppCompatActivity {
-
+    private TextView res;
+    private EditText n1,n2;
+    private Spinner operaciones;
+    private String op[];
+    private Resources resources;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {//onCreate -- metodo principal
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        res = (TextView) findViewById(R.id.lblResultado);
+        n1 = (EditText) findViewById(R.id.txtNumero1);
+        n2 = (EditText) findViewById(R.id.txtNumero2);
+        operaciones = (Spinner) findViewById(R.id.cmbOperaciones);
+        resources = this.getResources();
+        op = resources.getStringArray(R.array.opciones);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, op);
+        operaciones.setAdapter(adapter);
     }
 }
